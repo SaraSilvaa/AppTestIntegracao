685ccfb6fdc682dc099bdbc4ec840c667c2daee3
SaraSilvaa <141691931+SaraSilvaa@users.noreply.github.com>
2023-08-22 01:01:02 +0000

    Update 4 files

--- contas.js
+++ contas.js
@@ -1,21 +1,22 @@
-function soma(a,b)
-{
-    return a+b;
+function soma(a, b) {
+  return a + b;
 }
 
-function subtracao(a,b)
-{
-    return a-b;
+function subtracao(a, b) {
+  return a - b;
 }
 
-function divisao(a,b)
-{
-    return a/b;
+function divisao(a, b) {
+  return a / b;
 }
 
-function multiplicacao(a,b)
-{
-    return a*b;
+function multiplicacao(a, b) {
+  return a * b;
 }
 
-export {soma,subtracao,divisao,multiplicacao}
+module.exports = {
+  soma,
+  subtracao,
+  divisao,
+  multiplicacao,
+};
--- contas.test.js
+++ contas.test.js
@@ -1,5 +1,14 @@
-const contas = require('./contas');
+const contas = require("./contas");
 
-test("Soma 2+5 esperando 7 como resultado",()=>{
-    expect(contas.soma(2,5)).toBe(7)
-})
+test("Soma 2+5 esperando 7 como resultado", () => {
+  expect(contas.soma(2, 5)).toBe(7);
+});
+test("Subtracao 5-2 esperado 3 como resultado", () => {
+  expect(contas.subtracao(5, 2)).toBe(3);
+});
+test("Multiplicacao 5*2 esperado 10 como resultado", () => {
+  expect(contas.multiplicacao(5, 2)).toBe(10);
+});
+test("Divisao 5/2 esperado 2,5 como resultado", () => {
+  expect(contas.divisao(5, 2)).toBe(2.5);
+});
